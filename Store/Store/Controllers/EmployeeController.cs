0b44721cf790beead4aa0b27b0fe66759094f17b
unknown <jose.dlj15@gmail.com>
2023-04-16 22:44:48 -0600

    Employee crud added

---
+++ Store/Store/Controllers/EmployeeController.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.APIServices;
+using StoreModels.Models;
+using System.Xml.Linq;
+
+namespace StoreMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private string _apiControllerName = "Employees";
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public EmployeeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: EmployeeController
+        public async Task<IActionResult> Index()
+        {
+            using (var service = new CrudService<int, mEmployee>(_apiControllerName))
+            {
+                return View(await service.GetAll());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(mEmployee employee, IFormFile? PhotoPath)
+        {
+            //ModelState.Remove("PhotoPath");
+            ModelState.Remove("LoginId");
+            ModelState.Remove("Password");
+            if (ModelState.IsValid)
+            {
+                string wwwPath = _webHostEnvironment.WebRootPath;
+                if (PhotoPath != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(PhotoPath.FileName);
+                    string productPath = Path.Combine(wwwPath, @"storage\employees");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        PhotoPath.CopyTo(fileStream);
+                    }
+                    employee.PhotoPath = @"\storage\employees\" + fileName;
+                }
+                else
+                {
+                    employee.PhotoPath = @"\storage\employees\product_default.jpg";
+                }
+
+                using (var service = new CrudService<int, mEmployee>(_apiControllerName))
+                {
+                    employee.Password = "web2023";
+                    using (var serviceAux = new CrudService<int, mEmployee>(_apiControllerName))
+                    {
+                        var employees = await service.GetAll();
+                        var currentID = employee.FirstName.Trim().ToUpper() + employee.LastName.Trim().ToUpper();
+                        bool notExist = false;
+                        while (!notExist)
+                        {
+                            var test = employees.Where(x => x.LoginId == currentID).FirstOrDefault();
+                            if (test == null)
+                            {
+                                employee.LoginId = currentID;
+                                break;
+                            }
+                            else
+                            {
+                                currentID += new Random().Next(1, 9).ToString();
+                            }
+                        }
+                    }
+                    await service.Add(employee);
+                    TempData["success"] = "Employee created succesfully!";
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            using (var service = new CrudService<int, mEmployee>(_apiControllerName))
+            {
+                return View(await service.GetOne(id));
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, mEmployee employee, IFormFile? PhotoPath)
+        {
+            if (id != employee.EmployeeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                string wwwPath = _webHostEnvironment.WebRootPath;
+                if (PhotoPath != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(PhotoPath.FileName);
+                    string productPath = Path.Combine(wwwPath, @"storage\employees");
+
+                    //delete old image
+                    if (!string.IsNullOrEmpty(employee.PhotoPath))
+                    {
+                        var pathImageDelete = Path.Combine(wwwPath, employee.PhotoPath.TrimStart('\\'));
+                        if (System.IO.File.Exists(pathImageDelete))
+                        {
+                            System.IO.File.Delete(pathImageDelete);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        PhotoPath.CopyTo(fileStream);
+                    }
+                    employee.PhotoPath = @"\storage\employees\" + fileName;
+                }
+                using (var service = new CrudService<int, mEmployee>(_apiControllerName))
+                {
+                    using (var serviceAux = new CrudService<int, mEmployee>(_apiControllerName))
+                    {
+                        var employees = await service.GetAll();
+                        var currentID = employee.FirstName.Trim().ToUpper() + employee.LastName.Trim().ToUpper();
+                        bool notExist = false;
+                        while (!notExist)
+                        {
+                            var test = employees.Where(x => x.LoginId == currentID && x.EmployeeId != employee.EmployeeId).FirstOrDefault();
+                            if (test == null)
+                            {
+                                employee.LoginId = currentID;
+                                break;
+                            }
+                            else
+                            {
+                                currentID += new Random().Next(1, 9).ToString();
+                            }
+                        }
+                    }
+                    await service.Update(id, employee);
+                    TempData["success"] = "Employee updated succesfully!";
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            //delete image
+            using (var service = new CrudService<int, mEmployee>(_apiControllerName))
+            {
+                var employeeToDelete = await service.GetOne(id);
+                if (!string.IsNullOrEmpty(employeeToDelete.PhotoPath))
+                {
+                    string wwwPath = _webHostEnvironment.WebRootPath;
+                    var pathImageDelete = Path.Combine(wwwPath, employeeToDelete.PhotoPath.TrimStart('\\'));
+                    if (System.IO.File.Exists(pathImageDelete))
+                    {
+                        System.IO.File.Delete(pathImageDelete);
+                    }
+                }
+            }
+
+            //delete field
+            using (var service = new CrudService<int, mEmployee>(_apiControllerName))
+            {
+                TempData["success"] = "Employee deleted succesfully!";
+                await service.Delete(id);
+                //return RedirectToAction(nameof(Index));
+                return Json(new { success = true });
+            }
+        }
+    }
+}
---
+++ Store/StoreAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreAPI.Models;
+using StoreModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public EmployeesController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<mEmployee>>> GetEmployees()
+        {
+            if (_context.Employees == null)
+            {
+                return NotFound();
+            }
+            var listResult = new List<mEmployee>();
+            await _context.Employees.ForEachAsync(x => listResult.Add(new mEmployee()
+            {
+                EmployeeId = x.EmployeeId,
+                RoleId = x.RoleId,
+                FirstName = x.FirstName,
+                MiddleName = x.MiddleName,
+                LastName = x.LastName,
+                LoginId = x.LoginId,
+                Password = x.Password,
+                NationalNumberId = x.NationalNumberId,
+                BirthDate = x.BirthDate,
+                Gender = x.Gender,
+                PhotoPath = x.PhotoPath,
+            }));
+            return listResult;
+        }
+
+        // GET: api/Employees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<mEmployee>> GetEmployee(int id)
+        {
+            if (_context.Employees == null)
+            {
+                return NotFound();
+            }
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            var result = new mEmployee()
+            {
+                EmployeeId = employee.EmployeeId,
+                RoleId = employee.RoleId,
+                FirstName = employee.FirstName,
+                MiddleName = employee.MiddleName,
+                LastName = employee.LastName,
+                LoginId = employee.LoginId,
+                Password = employee.Password,
+                NationalNumberId = employee.NationalNumberId,
+                BirthDate = employee.BirthDate,
+                Gender = employee.Gender,
+                PhotoPath = employee.PhotoPath,
+            };
+            return result;
+        }
+
+        // PUT: api/Employees/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(int id, mEmployee employee)
+        {
+            if (id != employee.EmployeeId)
+            {
+                return BadRequest();
+            }
+
+            var employeeBase = _context.Employees.FirstOrDefault(x => x.EmployeeId == id);
+            if (employeeBase != null)
+            {
+                employeeBase.RoleId = employee.RoleId;
+                employeeBase.FirstName = employee.FirstName;
+                employeeBase.MiddleName = employee.MiddleName;
+                employeeBase.LastName = employee.LastName;
+                employeeBase.LoginId = employee.LoginId;
+                employeeBase.Password = employee.Password;
+                employeeBase.NationalNumberId = employee.NationalNumberId;
+                employeeBase.BirthDate = employee.BirthDate;
+                employeeBase.Gender = employee.Gender;
+                employeeBase.PhotoPath = employee.PhotoPath;
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employees
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<mEmployee>> PostEmployee(mEmployee employee)
+        {
+            if (_context.Employees == null)
+            {
+                return Problem("Entity set 'StoreContext.Employees'  is null.");
+            }
+            var employeeToInsert = new Employee()
+            {
+                EmployeeId = employee.EmployeeId,
+                RoleId = employee.RoleId,
+                FirstName = employee.FirstName,
+                MiddleName = employee.MiddleName,
+                LastName = employee.LastName,
+                LoginId = employee.LoginId,
+                Password = employee.Password,
+                NationalNumberId = employee.NationalNumberId,
+                BirthDate = employee.BirthDate,
+                Gender = employee.Gender,
+                PhotoPath = employee.PhotoPath,
+            };
+            _context.Employees.Add(employeeToInsert);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployee", new { id = employee.EmployeeId }, employee);
+        }
+
+        // DELETE: api/Employees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            if (_context.Employees == null)
+            {
+                return NotFound();
+            }
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return (_context.Employees?.Any(e => e.EmployeeId == id)).GetValueOrDefault();
+        }
+    }
+}
